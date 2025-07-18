c368650c5865efecd8d5bd4474e1e4cd3f419695
Rubén López <lopezrbn@gmail.com>
2025-07-18 02:48:43 +0200

    Code refactoring and review

--- config.py
+++ config.py
@@ -16,12 +16,10 @@ EARLY_STOP_PATIENCE = 4
 LOGGING_STEPS = 10
 RANDOM_SEED = 31415
 
-
-
 # PATHS
 PATH_BASE_DIR = os.path.dirname(__file__)
 PATH_DATA = os.path.join(PATH_BASE_DIR, "data")
-PATH_CHECKPOINTS = os.path.join(PATH_BASE_DIR, "checkpoints")
+PATH_CHECKPOINTS = os.path.join(PATH_BASE_DIR, "model_checkpoints")
 PATH_RESULTS = os.path.join(PATH_BASE_DIR, "results")
 
 PATH_DF_TRAIN = os.path.join(PATH_DATA, "original", "train.csv")
@@ -31,6 +29,3 @@ PATH_DF_TEST_LABELS = os.path.join(PATH_DATA, "original", "test_labels.csv")
 PATH_DATA_PROCESSED_DIR = os.path.join(PATH_DATA, "processed", MODEL_BASE)
 PATH_DS_TRAIN_TOKENIZED = os.path.join(PATH_DATA_PROCESSED_DIR, "ds_train_tokenized")
 PATH_DS_TEST_TOKENIZED = os.path.join(PATH_DATA_PROCESSED_DIR, "ds_test_tokenized")
-
-PATH_DF_SCORES = os.path.join(PATH_RESULTS, "evaluation", "df_scores.csv")
-PATH_PREDICTIONS_DICT = os.path.join(PATH_RESULTS, "predictions", "predictions.pkl")
